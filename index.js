f94205e30fdcb9dc2afef4934b9dced6f9fb7c45
Keith Chia <keith.chia@sparescnx.com>
2020-09-19 02:41:39 +0800

    fix: server return

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').createServer(app)
 var io = require('socket.io')(http)
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.send('ok')
 })
 
 io.on('connection', (socket) => {
--- remote.js
+++ remote.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000')
+const socket = io('https://fierce-tor-84554.herokuapp.com/')
 
 const btns = document.getElementsByClassName('btn')
 const sendBtn = document.getElementById('sendBtn')
